4104457c5853d097866c3789d18006bbb143e896
Jongha Lee <jongha8273@naver.com>
2026-04-29 20:29:10 +0900

    chore: gitignore build artifacts, results, pycache

--- src/blind_nav_system/build/blind_nav_system/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jongha/Desktop/GitHub/visually-impaired-navigation-robot/src/blind_nav_system/install/blind_nav_system'
